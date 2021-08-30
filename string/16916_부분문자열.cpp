056eeb828c8d4db2d81306b4d4bd42be0e8015fd
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-30 14:38:11 +0900

    210830

--- string/16916_부분문자열.cpp
+++ string/16916_부분문자열.cpp
@@ -3,9 +3,50 @@
 #include <string.h>
 using namespace std;
 
-int main(){
-    string input;
-    string check;
+int fail[1000001];
+string input;
+string check;
+
+void makeFail(){
+    int checkSize = check.size();
+    memset(fail, 0, sizeof(fail));
+    int j=0;
+    for (int i=1; i<checkSize; i++){
+        while(j > 0 && check[i] != check[j]){
+            j = fail[j-1];
+        }
+        if (check[i] == check[j]){
+            fail[i] = j+1;
+            j++;
+        }
+    }
     
-    for (int i=0; i<10000000; i++) cout<<i<<'\n';
+}
+void kmp(){
+    int inputSize = input.size();
+    int checkSize = check.size();
+    int j=0;
+    for (int i=0; i<inputSize; i++){
+        while(j>0 && input[i] != check[j]){
+            j = fail[j-1];
+        }
+        if (input[i] == check[j]){
+            if (j == checkSize-1){
+                cout<<1;
+                j = fail[j];
+                return;
+            } else {
+                j++;
+            }
+        }
+    }
+    cout<<0;
+    return;
+}
+
+int main(){
+    
+    cin>>input>>check;
+    makeFail();
+    kmp();
 }
